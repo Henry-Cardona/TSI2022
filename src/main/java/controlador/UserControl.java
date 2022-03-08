45333b9c0c91b9feffb0c4358a3f41eefe0f78a7
Henry-Cardona <henrycardona99@gmail.com>
2022-03-08 14:06:02 -0600

    CRUD de la entidad estado

--- src/main/java/controlador/UserControl.java
+++ src/main/java/controlador/UserControl.java
@@ -8,6 +8,9 @@ package controlador;
 import javax.persistence.EntityManager;
 import conexion.Conexion;
 import com.mycompany.tsi_web_accessdata.entity.Estado;
+import java.util.List;
+import javax.persistence.Query;
+import javax.xml.registry.infomodel.User;
 /**
  *
  * @author henry
@@ -16,8 +19,43 @@ import com.mycompany.tsi_web_accessdata.entity.Estado;
 public class UserControl {
     
     public void CrearUser(Estado id){
-        
+        EntityManager enti = entityManager();
+        try {
+            enti.getTransaction().begin();
+            enti.persist(id);
+            enti.getTransaction().commit();
+        } catch (Exception e) {
+            enti.getTransaction().rollback();
+        }
     }
+    
+    public void EditarUser(Estado id){
+        EntityManager enti = entityManager();
+        try {
+            enti.getTransaction().begin();
+            enti.merge(id);
+            enti.getTransaction().commit();
+        } catch (Exception e) {
+            enti.getTransaction().rollback();
+        }
+    }
+    
+    public void EliminarUser(Estado id){
+        EntityManager enti = entityManager();
+        try {
+            enti.getTransaction().begin();
+            enti.remove(id);
+            enti.getTransaction().commit();
+        } catch (Exception e) {
+            enti.getTransaction().rollback();
+        }
+    }
+    
+    public List<User> ExtraerUsuarios(){
+        Query query = entityManager().createQuery("Select * from User u");
+        return query.getResultList();
+    }
+    
     private EntityManager entityManager(){
         return Conexion.getInstacia().getFabrica().createEntityManager();
     }
