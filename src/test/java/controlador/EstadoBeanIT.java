cc20b2734419910095061e21270719e4ce0e4009
Henry-Cardona <henrycardona99@gmail.com>
2022-04-26 22:31:59 -0600

    modificacion del jenkinsfile 1.7

--- src/test/java/controlador/EstadoBeanIT.java
+++ src/test/java/controlador/EstadoBeanIT.java
@@ -6,17 +6,33 @@
 package controlador;
 
 import com.mycompany.tsi_web_accessdata.entity.Estado;
+import com.mycompany.tsi_web_accessdata.entity.JAXRSConfiguration;
+import java.io.StringReader;
+import java.net.URL;
+import java.util.Date;
 import java.util.List;
 import javax.ejb.embeddable.EJBContainer;
 import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 import javax.persistence.EntityManager;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.jupiter.api.Assertions;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -28,146 +44,134 @@ import org.junit.jupiter.api.extension.ExtendWith;
 public class EstadoBeanIT {
     
     @Deployment
+    @RunAsClient
     public static WebArchive crearDespliegue(){
         WebArchive salida = ShrinkWrap.create(WebArchive.class)
                 .addPackage("com.mycompany.tsi_web_accessdata.entity")
                 .addAsResource("persistence-arquillian.xml")
                 .addClass(AbstractDataAccesss.class)
                 .addClass(EstadoBeanTest.class)
-                .addAsResource("META-INF/persistence.xml","META-INF/persistence.xml")
-                .addAsResource("META-INF/sql/datos.sql","META-INF/sql/datos.sql")
+                .addClass(JAXRSConfiguration.class)
+                .addClass(EstadoBeanIT.class)
+                .addAsResource("jenkins/META-INF/persistence.xml","jenkins/META-INF/persistence.xml")
+                .addAsResource("jenkins/META-INF/sql/datos.sql","jenkins/META-INF/sql/datos.sql")
                 .addAsWebInfResource(EmptyAsset.INSTANCE,"beans.xml");
         System.out.println(salida.toString(true));
         return salida;
     }
     
-    @Inject
-    EstadoBean cut;
+    @ArquillianResource
+    URL url;
     
+    @Test
+    @Order(1)
+    @RunAsClient
     public void testFindAll(){
-        System.err.println("FindAll");
-        Assertions.assertNotNull(cut);
-        List<Estado> resultado = cut.findAll();
-        Assertions.assertNotNull(resultado);
-        Assertions.assertTrue(!resultado.isEmpty());
-        System.err.println("La lista posee" + resultado.size());
+        System.out.println("findAll");
+        int resultadoEsperado = 200;
+        Client cliente = ClientBuilder.newClient();
+        WebTarget target = cliente.target(url.toString()+"resources/");
+        Response respuesta = target.path("estado").request("application/json").get();
+        Assertions.assertEquals(resultadoEsperado, respuesta.getStatus());
+        String totalTexto = respuesta.getHeaderString("Total-registros");
+        Assertions.assertNotEquals(Integer.valueOf(0), Integer.valueOf(totalTexto));
+        String cuerpoString = respuesta.readEntity(String.class);
+        JsonReader lector = Json.createReader(new StringReader(cuerpoString));
+        JsonArray listaJson = lector.readArray();
+        int totalRegistros = listaJson.size();
+        Assertions.assertTrue(totalRegistros>0);
+        for(int i=0; i< listaJson.size(); i++){
+            JsonObject objeto = listaJson.getJsonObject(i);
+            System.out.println("ID: "+objeto.getInt("idEstado"));
+        }
     }
     
-//    
-//    public void testCrearDatos() throws Exception {
-//        System.out.println("controlador.EstadoBeanIT.testCrearDatos()");
-//        assertNotNull(cut);
-//        String esperado = EstadoBean.NOMBRE_USUARIO;
-//        String resultado = cut.getNombreUsuario(1);
-//        assertNotNull(resultado);
-//        assertEquals(esperado, resultado);
-//    }
-//
-//    /**
-//     * Test of modificarDatos method, of class EstadoBean.
-//     */
-//    @org.junit.Test
-//    public void testModificarDatos() throws Exception {
-//        System.out.println("modificarDatos");
-//        Estado nuevo = null;
-//        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-//        EstadoBean instance = (EstadoBean)container.getContext().lookup("java:global/classes/EstadoBean");
-//        instance.modificarDatos(nuevo);
-//        container.close();
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of eliminarDatos method, of class EstadoBean.
-//     */
-//    @org.junit.Test
-//    public void testEliminarDatos() throws Exception {
-//        System.out.println("eliminarDatos");
-//        Estado nuevo = null;
-//        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-//        EstadoBean instance = (EstadoBean)container.getContext().lookup("java:global/classes/EstadoBean");
-//        instance.eliminarDatos(nuevo);
-//        container.close();
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of findAll method, of class EstadoBean.
-//     */
-////    @org.junit.Test
-////    public void testFindAll() throws Exception {
-////        System.out.println("findAll");
-////        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-////        EstadoBean instance = (EstadoBean)container.getContext().lookup("java:global/classes/EstadoBean");
-////        List<Estado> expResult = null;
-////        List<Estado> result = instance.findAll();
-////        assertEquals(expResult, result);
-////        container.close();
-////        // TODO review the generated test code and remove the default call to fail.
-////    }
-//
-//    /**
-//     * Test of finRange method, of class EstadoBean.
-//     */
-//    @org.junit.Test
-//    public void testFinRange() throws Exception {
-//        System.out.println("finRange");
-//        int firts = 0;
-//        int pageSize = 0;
-//        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-//        EstadoBean instance = (EstadoBean)container.getContext().lookup("java:global/classes/EstadoBean");
-//        List<Estado> expResult = null;
-//        List<Estado> result = instance.finRange(firts, pageSize);
-//        assertEquals(expResult, result);
-//        container.close();
-//        // TODO review the generated test code and remove the default call to fail.
-//    }
-//
-//    /**
-//     * Test of findById method, of class EstadoBean.
-//     */
-//    @org.junit.Test
-//    public void testFindById() throws Exception {
-//        System.out.println("findById");
-//        Object id = null;
-//        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-//        EstadoBean instance = (EstadoBean)container.getContext().lookup("java:global/classes/EstadoBean");
-//        Estado expResult = null;
-//        Estado result = instance.findById(id);
-//        assertEquals(expResult, result);
-//        container.close();
-//        // TODO review the generated test code and remove the default call to fail.
-//    }
-//
-//    /**
-//     * Test of contar method, of class EstadoBean.
-//     */
-//    @org.junit.Test
-//    public void testContar() throws Exception {
-//        System.out.println("contar");
-//        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-//        EstadoBean instance = (EstadoBean)container.getContext().lookup("java:global/classes/EstadoBean");
-//        Long expResult = null;
-//        Long result = instance.contar();
-//        assertEquals(expResult, result);
-//        container.close();
-//        // TODO review the generated test code and remove the default call to fail.
-//    }
-//
-//    /**
-//     * Test of getEntityManager method, of class EstadoBean.
-//     */
-//    @org.junit.Test
-//    public void testGetEntityManager() throws Exception {
-//        System.out.println("getEntityManager");
-//        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-//        EstadoBean instance = (EstadoBean)container.getContext().lookup("java:global/classes/EstadoBean");
-//        EntityManager expResult = null;
-//        EntityManager result = instance.getEntityManager();
-//        assertEquals(expResult, result);
-//        container.close();
-//        // TODO review the generated test code and remove the default call to fail.
-//    }
+    @Test
+    @Order(2)
+    @RunAsClient
+    public void testFindById(){
+        System.out.println("findById");
+        int resultadoEsperado = 200;
+        Client cliente = ClientBuilder.newClient();
+        WebTarget target = cliente.target(url.toString()+"resources/");
+        Response respuesta = target.path("estado/2").request("application/json").get();
+        Assertions.assertEquals(resultadoEsperado, respuesta.getStatus());
+        String cuerpoString = respuesta.readEntity(String.class);
+        Assertions.assertTrue(!cuerpoString.isEmpty());
+        System.out.println(cuerpoString);
+    }
+    
+    @Test
+    @Order(3)
+    @RunAsClient
+    public void testContar(){
+        System.out.println("contar");
+        int resultadoEsperado = 200;
+        Client cliente = ClientBuilder.newClient();
+        WebTarget target = cliente.target(url.toString()+"resources/");
+        Response respuesta = target.path("estado/contar").request("application/json").get();
+        Assertions.assertEquals(resultadoEsperado, respuesta.getStatus());
+        String cuerpoString = respuesta.readEntity(String.class);
+        Assertions.assertTrue(!cuerpoString.isEmpty());
+        System.out.println("Tabla posee "+cuerpoString+" registros");
+    }
+    
+    @Test
+    @Order(4)
+    @RunAsClient
+    public void testCrear(){
+        System.out.println("crear");
+        int resultadoEsperado = 200;
+        Client cliente = ClientBuilder.newClient();
+        Estado nuevo = new Estado();
+        nuevo.setFechaCreacion(new Date());
+        nuevo.setNombre("Prueba");
+        nuevo.setObservaciones("Sin observacion");
+        nuevo.setIdEstado(8);
+        WebTarget target = cliente.target(url.toString()+"resources/");
+        Response respuesta = target.path("estado").request("application/json").post(Entity.json(nuevo));
+        System.out.println(respuesta.getStatus());
+        Assertions.assertEquals(resultadoEsperado, respuesta.getStatus());
+        String cuerpoString = respuesta.readEntity(String.class);
+        Assertions.assertTrue(!cuerpoString.isEmpty());
+        System.out.println(cuerpoString);
+        Response contar = target.path("estado/contar").request("application/json").get();
+        System.out.println("Tabla posee "+contar.readEntity(String.class)+" registros");
+    }
+    
+    @Test
+    @Order(5)
+    @RunAsClient
+    public void testModificar(){
+        System.out.println("modificar");
+        int resultadoEsperado = 200;
+        Client cliente = ClientBuilder.newClient();
+        Estado nuevo = new Estado();
+        nuevo.setFechaCreacion(new Date());
+        nuevo.setNombre("segunda prueba");
+        nuevo.setObservaciones("Sin observacion");
+        nuevo.setIdEstado(8);
+        WebTarget target = cliente.target(url.toString()+"resources/");
+        Response respuesta = target.path("estado").request("application/json").put(Entity.json(nuevo));
+        System.out.println(respuesta.getStatus());
+        Assertions.assertEquals(resultadoEsperado, respuesta.getStatus());
+        String cuerpoString = respuesta.readEntity(String.class);
+        Assertions.assertTrue(!cuerpoString.isEmpty());
+        System.out.println(cuerpoString);
+    }
+    
+    @Test
+    @Order(6)
+    @RunAsClient
+    public void testEliminar(){
+        System.out.println("eliminar");
+        int resultadoEsperado = 204;
+        Client cliente = ClientBuilder.newClient();
+        WebTarget target = cliente.target(url.toString()+"resources/");
+        Response respuesta = target.path("estado/8").request("application/json").delete();
+        System.out.println(respuesta.getStatus());
+        Assertions.assertEquals(resultadoEsperado, respuesta.getStatus());
+        Response contar = target.path("estado/contar").request("application/json").get();
+        System.out.println("Tabla posee "+contar.readEntity(String.class)+" registros");
+    }
 }
--- src/test/jenkins/META-INF.sql/datos.sql
+++
@@ -1,9 +0,0 @@
-
-/**
- * Author:  henry
- * Created: 3 abr. 2022
- */
-
-INSERT INTO tipo_objeto (activo, fecha_creacion) VALUES (true,'2022-03-01');
-INSERT INTO tipo_objeto (activo, fecha_creacion) VALUES (true,'2022-03-02');
-INSERT INTO tipo_objeto (activo, fecha_creacion) VALUES (true,'2022-03-03');
