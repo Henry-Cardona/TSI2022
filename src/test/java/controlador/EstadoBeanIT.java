c2d22066f738c81bba8af29c4070c18a871e2820
Henry-Cardona <henrycardona99@gmail.com>
2022-04-28 13:51:24 -0600

    modificacion del jenkinsfile 2.1

--- src/test/java/controlador/EstadoBeanIT.java
+++ src/test/java/controlador/EstadoBeanIT.java
@@ -42,7 +42,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
  */
 @ExtendWith(ArquillianExtension.class)
 public class EstadoBeanIT {
-    //esto es una prueba d enuevo
+    //esto es una prueba d enuevo y otra vez
 //    @Deployment
 //    @RunAsClient
 //    public static WebArchive crearDespliegue(){
